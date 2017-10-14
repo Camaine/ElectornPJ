a8fdea9c0d3426e9dadce8004b716f46f037c9d0
Juwon Kim <kang9290@gmail.com>
2017-10-14 16:23:12 -0500

    commit

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php 
-echo "Hello World!"; 
+echo "Hello World!";
+echo "MySql Connect test<br>";
+      $db = mysqli_connect("myprojectdb.cvmjrshfk7ix.us-east-2.rds.amazonaws.com", "juwonkim", "5gkrsus5qks", "records");
+
+      if($db){
+          echo "connect : success<br>";
+      }
+      else{
+          echo "disconnect : fail<br>";
+      }
 ?>
