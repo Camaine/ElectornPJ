4a4866baa8cef5702b2170c15ffd8632ff2070fc
Juwon Kim <kang9290@gmail.com>
2017-10-14 16:10:04 -0500

    commit

---
+++ scripts/sqlMng.php
@@ -0,0 +1,11 @@
+<?php
+      echo "MySql Connect test<br>";
+      $db = mysqli_connect("myprojectdb.cvmjrshfk7ix.us-east-2.rds.amazonaws.com", "juwonkim", "5gkrsus5qks", "records");
+
+      if($db){
+          echo "connect : success<br>";
+      }
+      else{
+          echo "disconnect : fail<br>";
+      }
+?>
