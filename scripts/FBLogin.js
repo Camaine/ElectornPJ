54fbffd77810dfa649031546236f79bed2038ead
Juwon Kim <kang9290@gmail.com>
2017-10-14 15:01:12 -0500

    login test

---
+++ scripts/FBLogin.js
@@ -0,0 +1,18 @@
+var appId = '363763484061000';
+
+var fbUserId;
+var fbUserEmail;
+var fbUserPhone;
+
+
+function FaceBookInit(){
+
+};
+
+
+/*!
+ * Login to your application using Facebook.
+ * Uses the Facebook SDK for JavaScript available here:
+ * https://developers.facebook.com/docs/javascript/quickstart/
+ */
+
