370a90dea5683f2e2641a82da280735dfaf581a7
Juwon Kim <kang9290@gmail.com>
2017-10-20 23:30:47 -0500

    login test

--- login.php
+++ login.php
@@ -2,6 +2,11 @@
 $db = mysqli_connect("myprojectdb.cvmjrshfk7ix.us-east-2.rds.amazonaws.com", "juwonkim", "5gkrsus5qks", "records");
 mysqli_query($db, "INSERT INTO LOG (email,phone) VALUES('".$_GET['myemail']."', '".$_GET['myphone']."')");
 $getid = mysqli_query($db, "SELECT id FROM LOG");
-echo $getid;
+$row = mysqli_fetch_row($getid);
+<script>
+    var myid = "<?php echo $row[0]; ?>";
+    console.log(myid);
+    localStorage.setItem("myid",myid);
+</script>
 mysqli_close($db);
 ?>
