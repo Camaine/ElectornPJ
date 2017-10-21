023fe40831fd957313383c25a35d11adb0981430
Juwon Kim <kang9290@gmail.com>
2017-10-20 23:34:02 -0500

    commit

--- login.php
+++ login.php
@@ -3,6 +3,7 @@ $db = mysqli_connect("myprojectdb.cvmjrshfk7ix.us-east-2.rds.amazonaws.com", "ju
 mysqli_query($db, "INSERT INTO LOG (email,phone) VALUES('".$_GET['myemail']."', '".$_GET['myphone']."')");
 $getid = mysqli_query($db, "SELECT id FROM LOG");
 $row = mysqli_fetch_row($getid);
+echo $row[0];
 <script>
     var myid = "<?php echo $row[0]; ?>";
     console.log(myid);
