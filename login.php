73fe14f03921e0c7cc0c32158287470b1379d303
Juwon Kim <kang9290@gmail.com>
2017-11-01 17:45:10 -0500

    commit

--- login.php
+++ login.php
@@ -7,21 +7,18 @@ use Aws\Sns\SnsClient;
 $sns = SnsClient::factory(array(
 
     'region' => 'us-east-2', //Change according to you
-    'version' => '2010-03-31', //Change according to you
-    'credentials' => [
+    'profile' => [
         'key'    => 'AKIAIIBZ3GIZSS4S6PBQ',
         'secret' => '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',
-    ],
-    'scheme' => 'http', //disables SSL certification, there was an error on enabling it
-
+    ]
 ));
-$result = $sns->subscribe(array(
+/*$result = $sns->subscribe(array(
     // TopicArn is required
     'TopicArn' => 'arn:aws:sns:us-east-2:186502234717:juwonkim',
     // Protocol is required
     'Protocol' => 'email',
     'Endpoint' => '".$_GET['myemail']."'
-));
+));*/
 echo $row[0];
 echo '<script type="text/javascript">';
 echo 'var myid = ' .$row[0].';';
