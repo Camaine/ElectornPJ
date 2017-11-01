4c96385330fcbca01c9b2d4b83f643c6b6daaff6
Juwon Kim <kang9290@gmail.com>
2017-11-01 17:35:24 -0500

    add aws sns service

--- login.php
+++ login.php
@@ -3,6 +3,25 @@ $db = mysqli_connect("myprojectdb.cvmjrshfk7ix.us-east-2.rds.amazonaws.com", "ju
 mysqli_query($db, "INSERT INTO LOG (email,phone) VALUES('".$_GET['myemail']."', '".$_GET['myphone']."')");
 $getid = mysqli_query($db, "SELECT id FROM LOG WHERE email = '".$_GET['myemail']."'");
 $row = mysqli_fetch_row($getid);
+use Aws\Sns\SnsClient;
+$sns = new SnsClient([
+
+    'region' => 'us-east-2', //Change according to you
+    'version' => '2010-03-31', //Change according to you
+    'credentials' => [
+        'key'    => 'AKIAIIBZ3GIZSS4S6PBQ',
+        'secret' => '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',
+    ],
+    'scheme' => 'http', //disables SSL certification, there was an error on enabling it
+
+]);
+$result = $sns->subscribe(array(
+    // TopicArn is required
+    'TopicArn' => 'arn:aws:sns:us-east-2:186502234717:juwonkim',
+    // Protocol is required
+    'Protocol' => 'email',
+    'Endpoint' => '".$_GET['myemail']."'
+));
 echo $row[0];
 echo '<script type="text/javascript">';
 echo 'var myid = ' .$row[0].';';
