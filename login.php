213c74598acbeaa582a0c1b9e98d966208d78151
Juwon Kim <kang9290@gmail.com>
2017-11-01 17:40:29 -0500

    commit

--- login.php
+++ login.php
@@ -4,7 +4,7 @@ mysqli_query($db, "INSERT INTO LOG (email,phone) VALUES('".$_GET['myemail']."', 
 $getid = mysqli_query($db, "SELECT id FROM LOG WHERE email = '".$_GET['myemail']."'");
 $row = mysqli_fetch_row($getid);
 use Aws\Sns\SnsClient;
-$sns = new SnsClient([
+$sns = SnsClient::factory(array(
 
     'region' => 'us-east-2', //Change according to you
     'version' => '2010-03-31', //Change according to you
@@ -14,7 +14,7 @@ $sns = new SnsClient([
     ],
     'scheme' => 'http', //disables SSL certification, there was an error on enabling it
 
-]);
+));
 $result = $sns->subscribe(array(
     // TopicArn is required
     'TopicArn' => 'arn:aws:sns:us-east-2:186502234717:juwonkim',
