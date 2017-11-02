fe59e2b00d808aeaa49a82a35b25b704f97d47f1
Juwon Kim <kang9290@gmail.com>
2017-11-01 19:31:16 -0500

    commit

--- login.php
+++ login.php
@@ -22,13 +22,13 @@ $sns = new SnsClient([
     'scheme' => 'http', //disables SSL certification, there was an error on enabling it
 
 ]);
-$result = $sns->subscribe(array(
+/*$result = $sns->subscribe(array(
     // TopicArn is required
     'TopicArn' => 'arn:aws:sns:us-east-2:186502234717:juwonkim',
     // Protocol is required
     'Protocol' => 'email',
     'Endpoint' => 'kang9290@gmail.com'
-));
+));*/
 echo 'result is';
 echo $result;
 ?>
