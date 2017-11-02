2af2ef25de2bc62c172bde800d9b7000d5e80874
Juwon Kim <kang9290@gmail.com>
2017-11-02 10:26:52 -0500

    commit

--- login.php
+++ login.php
@@ -11,28 +11,18 @@ echo 'localStorage.clear();';
 echo 'localStorage.setItem("myid",myid);';
 echo '</script>';
 mysqli_close($db);
-use Aws\Sns\SnsClient;
-use Aws\Credentials\CredentialProvider;
-$provider = CredentialProvider::defaultProvider();
-$sns = snsClient::factory(array(
-
+$sns = new Aws\Sns\SnsClient([
     'region' => 'us-east-2', //Change according to you
     'version' => 'latest', //Change according to you
-    'credentials' => $provider,
-    'scheme' => 'http', //disables SSL certification, there was an error on enabling it
-
-));
-
-$result = $sns -> createTopic([
-    'Name' => 'Testing',
 ]);
-/*$result = $sns->subscribe(array(
+
+$result = $sns->subscribe(array(
     // TopicArn is required
     'TopicArn' => 'arn:aws:sns:us-east-2:186502234717:juwonkim',
     // Protocol is required
     'Protocol' => 'email',
-    'Endpoint' => 'kang9290@gmail.com'
-));*/
+    'Endpoint' => '".$_GET['myemail']."'
+));
 echo 'result is';
 if ($result)
     echo "Yes";
