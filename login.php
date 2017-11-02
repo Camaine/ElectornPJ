b8465e77c44b8c738b5eaa0177d1a1ad2e48ec9f
Juwon Kim <kang9290@gmail.com>
2017-11-02 10:53:13 -0500

    commit

--- login.php
+++ login.php
@@ -12,22 +12,7 @@ echo 'localStorage.setItem("myid",myid);';
 echo '</script>';
 mysqli_close($db);
 
-require './vendor/autoload.php';
-$sns = new Aws\Sns\SnsClient([
-    'region' => 'us-east-2', //Change according to you
-    'version' => 'latest', //Change according to you
-]);
+$email = $_GET['myemail'];
+inclue_file "callsns.php";
 
-$result = $sns->subscribe(array(
-    // TopicArn is required
-    'TopicArn' => 'arn:aws:sns:us-east-2:186502234717:juwonkim',
-    // Protocol is required
-    'Protocol' => 'email',
-    'Endpoint' => 'kang9290@gmail.com'
-));
-echo 'result is';
-if ($result)
-    echo "Yes";
-else
-    echo "No";
 ?>
