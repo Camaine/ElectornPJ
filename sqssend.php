3ca4fba97d9456d37f59cb85b149e870f6500f4f
Juwon Kim <kang9290@gmail.com>
2017-11-07 21:37:57 -0600

    commit

--- sqssend.php
+++ sqssend.php
@@ -1,26 +1,31 @@
 <?php
 require './vendor/autoload.php';
+use Aws\Exception\AwsException;
 $sqs = new Aws\Sqs\SqsClient([
     'region' => 'us-east-2',
     'version' => 'latest'
 ]);
 
 echo $_GET['id'];
-
-$result = $sqs->sendMessage([
-    'MessageAttributes' => [
-    '<String>' => [
-            'DataType' => 'String' // REQUIRED
+try{
+    $result = $sqs->sendMessage([
+        'MessageAttributes' => [
+            '<String>' => [
+                'DataType' => 'String' // REQUIRED
+            ],
+            // ...
         ],
-        // ...
-    ],
-    'MessageBody' => $_GET['id'], // REQUIRED
-    'QueueUrl' => 'https://sqs.us-east-2.amazonaws.com/186502234717/itmo-juwonkim' // REQUIRED
-]);
-echo 'result is';
-if ($result)
-    echo "Yes";
-else
-    echo "No";
+        'MessageBody' => $_GET['id'], // REQUIRED
+        'QueueUrl' => 'https://sqs.us-east-2.amazonaws.com/186502234717/itmo-juwonkim' // REQUIRED
+    ]);
+    echo 'result is';
+    if ($result)
+        echo "Yes";
+    else
+        echo "No";
+}catch(AwsException $e){
+    echo "Fail";
+}
+
 
 ?>
