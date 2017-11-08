62dbc6bdab5f12fd0c61f7bd120ada8dcb75d88f
Juwon Kim <kang9290@gmail.com>
2017-11-07 20:46:51 -0600

    commit

--- sqssend.php
+++ sqssend.php
@@ -1,10 +1,3 @@
-
-/**
- * Created by IntelliJ IDEA.
- * User: juwonkim
- * Date: 2017. 11. 7.
- * Time: 오후 4:54
-**/
 <?php
 require './vendor/autoload.php';
 $sqs = new Aws\Sqs\SqsClient([
@@ -12,10 +5,12 @@ $sqs = new Aws\Sqs\SqsClient([
     'version' => 'latest'
 ]);
 
+echo $_GET['id'];
+
 $result = $sqs->sendMessage([
     'MessageAttributes' => [
     '<String>' => [
-            'DataType' => 'string' // REQUIRED
+            'DataType' => 'String' // REQUIRED
         ],
         // ...
     ],
