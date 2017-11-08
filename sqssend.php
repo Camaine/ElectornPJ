e20ca881ccd9340870cb383518aaa3905e0d8c39
Juwon Kim <kang9290@gmail.com>
2017-11-07 20:52:13 -0600

    commit

--- sqssend.php
+++ sqssend.php
@@ -17,6 +17,10 @@ $result = $sqs->sendMessage([
     'MessageBody' => $_GET['id'], // REQUIRED
     'QueueUrl' => 'https://sqs.us-east-2.amazonaws.com/186502234717/itmo-juwonkim' // REQUIRED
 ]);
-
+echo 'result is';
+if ($result)
+    echo "Yes";
+else
+    echo "No";
 
 ?>
