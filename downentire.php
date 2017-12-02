9262fd95aaf379961780af232c4deb30e90ba73d
Juwon Kim <kang9290@gmail.com>
2017-12-01 23:08:55 -0600

    commit

--- downentire.php
+++ downentire.php
@@ -4,12 +4,13 @@ use Aws\S3\S3Client;
 use Aws\Exception\AwsException;
 use Aws\Credentials\CredentialProvider;
 
-$provider = CredentialProvider::defaultProvider();
+$provider = CredentialProvider::ini();
+$provider = CredentialProvider::memoize($provider);
 //Create a S3Client
 $s3Client = new S3Client([
     'region' => 'us-west-2',
     'version' => '2006-03-01',
-    'provider' => 'default'
+    'credentials' => $provider
 ]);
 
 echo "Credential OK";
