765b389626b416aa54a3da5a7de4d82182d03384
Juwon Kim <kang9290@gmail.com>
2017-12-01 21:40:08 -0600

    commit

--- MngDbAct.php
+++ MngDbAct.php
@@ -2,10 +2,10 @@
 
 $db = mysqli_connect("myprojectdb.cvmjrshfk7ix.us-east-2.rds.amazonaws.com", "juwonkim", "5gkrsus5qks", "records");
 if($_GET['act'] == "1"){
-    $deactivate = mysqli_query($db, "UPDATE LOG set decativate = 0 WHERE id = '".$_GET['id']."'");
+    $deactivate = mysqli_query($db, "UPDATE LOG set deactivate = 0 WHERE id = '".$_GET['id']."'");
 
 }else{
-    $deactivate = mysqli_query($db, "UPDATE LOG set decativate = 1 WHERE id = '".$_GET['id']."'");
+    $deactivate = mysqli_query($db, "UPDATE LOG set deactivate = 1 WHERE id = '".$_GET['id']."'");
 }
 mysqli_close($db);
 ?>
